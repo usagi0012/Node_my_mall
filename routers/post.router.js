7250831c3b0638d69e56e8813c1e254624f6b1ad
jiwon <usagi0012@naver.com>
2023-11-19 18:44:45 +0900

    상품 목록 조회 API

--- routers/post.router.js
+++ routers/post.router.js
@@ -111,4 +111,22 @@ router.delete('/posts/:postId', authMiddleware, async (req, res) => {
     res.status(200).json({ Message: '상품을 삭제하였습니다.' });
 });
 
+//상품 목록 조회 API
+router.get('/posts', async (req, res) => {
+    //최근에 수정된(업데이트된)순으로 정렬해서 조회하도록 함
+    const posts = await Posts.findAll({
+        attributes: [
+            'postId',
+            'title',
+            'content',
+            'userId',
+            'status',
+            'createdAt',
+            'updatedAt',
+        ],
+        order: [['updatedAt', 'desc']],
+    });
+    res.status(200).json({ posts });
+});
+
 module.exports = router;
